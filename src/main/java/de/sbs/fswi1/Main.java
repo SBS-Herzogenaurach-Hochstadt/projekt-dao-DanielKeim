d02f5f1116a352345e3b8ac661bc15577c19a8f3
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-05-14 18:20:54 +0200

    Update .gitignore, add packaging jar to pom, make dao-Object and call findAll()

--- src/main/java/de/sbs/fswi1/Main.java
+++ src/main/java/de/sbs/fswi1/Main.java
@@ -1,7 +1,15 @@
 package de.sbs.fswi1;
 
+
+import java.util.List;
+
+import de.sbs.fswi1.models.StudentDTO;
+import de.sbs.fswi1.services.DataAccessObject;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Pfad muss angepasst werden
+        DataAccessObject dao = new DataAccessObject("C:\\Users\\cgg\\Documents\\data\\studenten.csv");
+        List<StudentDTO> studenten = dao.findAll();
     }
 }
