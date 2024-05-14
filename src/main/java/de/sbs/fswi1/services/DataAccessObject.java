878c8f630e71c37364a92033d88eb33381787bfe
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-05-14 10:54:47 +0200

    Erestellung der Klassen

---
+++ src/main/java/de/sbs/fswi1/services/DataAccessObject.java
@@ -0,0 +1,79 @@
+package de.sbs.fswi1.services;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import de.sbs.fswi1.models.StudentDTO;
+
+public class DataAccessObject {
+
+    // C:/Users/cgg/Documents/data
+    private String pathAsString;
+
+    public DataAccessObject(String pathAsString) {
+        this.pathAsString = pathAsString;
+    }
+
+    public List<StudentDTO> findAll() {
+        // Zugriff auf eine gespeicherte Datei benötigt IMMER einen PATH
+        // Hier als relativer Pfad
+        Path pfadAufCSV = Path.of(pathAsString);
+
+        // Wir machen nur etwas, wenn diw Datei wirklich existiert
+        if (Files.exists(pfadAufCSV)) {
+
+            // FehlerBEHANDLUNG, weil die Methode readString mehrer Exceptions wirft, wenn
+            // etwas nicht klappt
+            // Bei Zugriff auf das Dateiensystem fordert Java try - catch
+            try {
+                // ALLES als Zeichenfolge einlesen
+                String csvContent = Files.readString(pfadAufCSV);
+
+                // Zeichenfolge am Zeilenumbruchzeichen "\n" zerlegen
+                // und als Array zurückgeben
+                String[] lines = csvContent.split("\n");
+
+                // leere List instanziieren, um in und nach if Zugriff darauf zu haben
+                List<StudentDTO> studenten = new ArrayList<>();
+
+                // Performance, wenn nichts aus der Datei angekommen ist, dann mach nichts
+                if (lines.length > 0) {
+
+                    // Mittels for-schleife über alle Datenzeilen gehen
+                    for (int i = 0; i < lines.length; i++) {
+                        // jeweils die aktuelle Datenzeile am Komma zerlegen,
+                        // um ein Array mit den einzelnen Elementen zu bekommen
+                        String[] elements = lines[i].split(",");
+
+                        // Erzeugung einer Instanz der Klasse Student mit den gespeicherten Werte aus
+                        // dem Array elements, das mit der .split(",") erzeugt wurde
+                        StudentDTO lokStudent = new StudentDTO(elements[0], elements[1], elements[2], elements[3]);
+
+                        // Aufnaheme des Objekts Student (lokStudent) in die Liste
+                        studenten.add(lokStudent);
+
+                        // Alternative
+                        // studenten.add(new StudentDTO(elements[0], elements[1], elements[2], elements[3]));
+                    }
+                }
+
+                return studenten;
+                // ACHTUNG: Nach return endet immer ein Codebereich und die Methode springt
+                // zurück zum Aufrufer
+
+            } catch (IOException e) {
+
+                // Ausgabe einer möglichen Fehlermeldung auf der Console
+                e.printStackTrace();
+            }
+        }
+
+        // Sollte alles nicht geklappt haben, dann gibt eine null-Referenz an den
+        // Aufrufer zurück!
+        // BEACHTE: Es kann mehrere returns gebe, ABER immer entweder oder erreichbar
+        return null;
+    }
+}
