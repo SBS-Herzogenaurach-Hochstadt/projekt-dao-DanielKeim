c749c81cbc2ae710daf762b304a8f4e2c152e484
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-06-03 21:25:26 +0200

    Umbau für Lösungspräsentation und Tippfehler entfernt

--- src/main/java/de/sbs/fswi1/Main.java
+++ src/main/java/de/sbs/fswi1/Main.java
@@ -1,18 +1,21 @@
 package de.sbs.fswi1;
 
+import java.util.List;
+
 import de.sbs.fswi1.models.Student;
+import de.sbs.fswi1.models.StudentDTO;
+import de.sbs.fswi1.services.DataAccessObject;
 
 public class Main {
     public static void main(String[] args) {
         // Pfad muss angepasst werden
-        /*
         DataAccessObject dao = new DataAccessObject("C:\\Users\\cgg\\Documents\\data\\studenten.csv");
         List<StudentDTO> studenten = dao.findAll();
 
         for (StudentDTO studentDTO : studenten) {
             System.out.println(studentDTO);
         }
-        */
+        
         Student std1 = new Student("Peter", "Lustig", "12.12.2000", "FSWI-1");
         System.out.println(std1.getAlterInJahrenZuHeute());
     }
--- src/main/java/de/sbs/fswi1/services/DataAccessObject.java
+++ src/main/java/de/sbs/fswi1/services/DataAccessObject.java
@@ -2,7 +2,6 @@ package de.sbs.fswi1.services;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,44 +9,25 @@ import de.sbs.fswi1.models.StudentDTO;
 
 public class DataAccessObject {
 
-    private final String pathAsString;
+	private final String pathAsString;
+	private final List<StudentDTO> studenten;
 
-    public DataAccessObject(String pathAsString) {
-        this.pathAsString = pathAsString;
-    }
+	public DataAccessObject(String pathAsString) {
+		this.pathAsString = pathAsString;
+		studenten = new ArrayList<>();
+	}
 
-    public boolean save(StudentDTO saveObject) {
-        Path pfadAufCSV = Path.of(pathAsString);
-        if (Files.exists(pfadAufCSV)) {
-            try {
-                String content = String.format("%s,%s,%s,%s%n", saveObject.getVorname(), saveObject.getNachname(), saveObject.getGeburtsdatum(), saveObject.getKlasse());
-                Files.writeString(pfadAufCSV, content, StandardOpenOption.APPEND);
-                return true;
-            } catch (Exception ignored) {
-            }
-        }
-        return false;
-    }
-
-    public List<StudentDTO> findAll() {
-        Path pfadAufCSV = Path.of(pathAsString);
-        if (Files.exists(pfadAufCSV)) {
-            try {
-                String csvContent = Files.readString(pfadAufCSV);
-                return getStudentDTOS(csvContent);
-            } catch (Exception ignored) {
-            }
-        }
-        return null;
-    }
-
-    private static List<StudentDTO> getStudentDTOS(String csvContent) {
-        String[] lines = csvContent.split("\n");
-        List<StudentDTO> studenten = new ArrayList<>();
-        for (String line : lines) {
-            String[] elements = line.split(",");
-            studenten.add(new StudentDTO(elements[0], elements[1], elements[2],elements[3]));
-        }
-        return studenten;
-    }
+	public List<StudentDTO> findAll() {
+		Path pfadAufCSV = Path.of(pathAsString);
+		try {
+			String csvContent = Files.readString(pfadAufCSV);
+			String[] lines = csvContent.split("\n");
+			for (String line : lines) {
+				String[] elements = line.split(",");
+				studenten.add(new StudentDTO(elements[0], elements[1], elements[2], elements[3]));
+			}
+		} catch (Exception ignored) {
+		}
+		return studenten;
+	}
 }
