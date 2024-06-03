50d961aae55fde3c3755801df7d0e7d90f783ccf
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-06-03 20:54:54 +0200

    Anlegen Student

--- src/main/java/de/sbs/fswi1/Main.java
+++ src/main/java/de/sbs/fswi1/Main.java
@@ -1,28 +1,19 @@
 package de.sbs.fswi1;
 
-import java.util.List;
-
-import de.sbs.fswi1.models.StudentDTO;
-import de.sbs.fswi1.services.DataAccessObject;
+import de.sbs.fswi1.models.Student;
 
 public class Main {
     public static void main(String[] args) {
         // Pfad muss angepasst werden
+        /*
         DataAccessObject dao = new DataAccessObject("C:\\Users\\cgg\\Documents\\data\\studenten.csv");
         List<StudentDTO> studenten = dao.findAll();
 
         for (StudentDTO studentDTO : studenten) {
             System.out.println(studentDTO);
         }
-   
-        /*
-        System.out.println(studenten.size());
-
-        if(dao.save(studenten.get(4))) {
-            studenten = dao.findAll();
-        }
-
-        System.out.println(studenten.size());
-         */
+        */
+        Student std1 = new Student("Peter", "Lustig", "12.12.2000", "FSWI-1");
+        System.out.println(std1.getAlterInJahrenZuHeute());
     }
 }
