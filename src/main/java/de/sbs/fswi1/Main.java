201e1be1db3b18332a0aa4adbadf7950d661e0d2
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-06-04 10:48:20 +0200

    Warnungen entfernt

--- src/main/java/de/sbs/fswi1/Main.java
+++ src/main/java/de/sbs/fswi1/Main.java
@@ -2,7 +2,6 @@ package de.sbs.fswi1;
 
 import java.util.List;
 
-import de.sbs.fswi1.models.Mensch;
 import de.sbs.fswi1.models.Student;
 import de.sbs.fswi1.models.StudentDTO;
 import de.sbs.fswi1.services.DataAccessObject;
@@ -13,26 +12,22 @@ public class Main {
         DataAccessObject dao = new DataAccessObject("C:\\Users\\cgg\\Documents\\data\\studenten.csv");
         List<StudentDTO> studenten = dao.findAll();
 
-        /*
         for (StudentDTO studentDTO : studenten) {
             System.out.println(studentDTO);
         }
-         */
         
         Student std1 = new Student("Peter", "Lustig", "12.12.2000", "FSWI-1");
         Student std2 = new Student("Peter", "Lustig", "12.12.2000", "FSWI-2");
         StudentDTO std3 = new StudentDTO("Peter", "Lustig", "12.12.2000", "FSWI-1");
         StudentDTO std4 = new StudentDTO("Peter", "Lustiger", "12.12.2000", "FSWI-1");
 
+        // Aufruf der save-Methode, um die Werte des Studenten std4 in die Studenten.csv zu speichern 
         dao.save(std4);
-
-
 
         System.out.println(std1.hashCode());
         System.out.println(std2.hashCode());
         System.out.println(std3.hashCode());
         System.out.println(std4.hashCode());
         System.out.println(std1.equals(std2));
-
     }
 }
