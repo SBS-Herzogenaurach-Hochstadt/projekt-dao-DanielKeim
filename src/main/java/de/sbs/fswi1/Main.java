d1fb38bbbf3d48cc873f008ce73ca409582fb546
Claus Gumbmann <claus.gumbmann@schule.bayern.de>
2024-06-11 11:58:38 +0200

    Methode createJson

--- src/main/java/de/sbs/fswi1/Main.java
+++ src/main/java/de/sbs/fswi1/Main.java
@@ -14,45 +14,79 @@ import de.sbs.fswi1.services.DataAccessObject;
 
 @SuppressWarnings("unused")
 public class Main {
-
-    static DataAccessObject dao;
+    // Klassen-Scope
 
     public static void main(String[] args) {
-
-        // Pfad muss angepasst werden
-        // dao = new DataAccessObject("C:\\Users\\cgg\\Documents\\data\\studenten.csv");
+        // main-Methoden-Scope
 
         Path pfadAufCSV = Path.of("C:/Users/cgg/Documents/data/studenten.csv");
         try {
             String csvContent = Files.readString(pfadAufCSV);
-
             if (csvContent != null) {
                 createJson(csvContent);
             }
-
         } catch (Exception ignored) {
         }
 
-        /*
-         * List<StudentDTO> studenten = dao.findAll();
-         * 
-         * for (StudentDTO studentDTO : studenten) {
-         * System.out.println(studentDTO);
-         * }
-         * 
-         * Student std1 = new Student("Peter", "Lustig", "12.12.2000", "FSWI-1");
-         * Student std2 = new Student("Peter", "Lustig", "12.12.2000", "FSWI-1");
-         * 
-         * System.out.println(std2.hashCode());
-         * System.out.println(((Object)std2).hashCode());
-         */
+        createJsonAusListe();
+    }
+    /*
+     * List<StudentDTO> studenten = dao.findAll();
+     * Pfad muss angepasst werden
+     * dao = new DataAccessObject("C:\\Users\\cgg\\Documents\\data\\studenten.csv");
+     * for (StudentDTO studentDTO : studenten) {
+     * System.out.println(studentDTO);
+     * }
+     * 
+     * Student std1 = new Student("Peter", "Lustig", "12.12.2000", "FSWI-1");
+     * Student std2 = new Student("Peter", "Lustig", "12.12.2000", "FSWI-1");
+     * 
+     * System.out.println(std2.hashCode());
+     * System.out.println(((Object)std2).hashCode());
+     */
+
+    private static void createJsonAusListe() {
+
+        // im Kopf Schritte bestimmen, die nötig sind!!!!!!
+        // Schritt 1: DAO erzeugen
+        DataAccessObject dao = new DataAccessObject("C:/Users/cgg/Documents/data/studenten.csv");
+
+        // Schrtitt 2: dao bnutzen, um List<Student>
+        List<StudentDTO> liste = dao.findAll();
+
+        // Schritt 3
+        // new StringBuilder-Objekt vorbelegt mit "["
+        StringBuilder json = new StringBuilder("[");
+
+        // Schritt 4
+        // Festlegung "Key":"Value" mit Platzhalter %s für das spätere Value
+        String format = "{\"vorname\":\"%s\",\"nachname\":\"%s\",\"geburtsdatum\":\"%s\",\"klasse\":\"%s\"}";
+        
+        // Schritt 5 Schleife 
+        for (int i = 0; i < liste.size(); i++) {
+            // Zugriff auf die StudentenDTO-Objekte der Liste über den Index der Schleife         
+            json.append(String.format(format, liste.get(i).getVorname(),
+            liste.get(i).getNachname(),
+            liste.get(i).getGeburtsdatum(),
+            liste.get(i).getKlasse()));
+            
+            if (i < liste.size() - 1) {
+                json.append(",");
+            }
+        }
+        json.append("]");
+
+       
+
     }
 
     private static void createJson(String csvContent) {
         String[] lines = csvContent.split("\n");
 
+        // new StringBuilder-Objekt vorbelegt mit "["
         StringBuilder json = new StringBuilder("[");
 
+        // Festlegung "Key":"Value" mit Platzhalter %s für das spätere Value
         String format = "{\"vorname\":\"%s\",\"nachname\":\"%s\",\"geburtsdatum\":\"%s\",\"klasse\":\"%s\"}";
 
         for (int i = 0; i < lines.length; i++) {
@@ -64,7 +98,6 @@ public class Main {
                 json.append(",");
             }
         }
-
         json.append("]");
 
         try {
